8fe9d3fd2344aeb974ff2516c06192e11ba3b846
flor <florenciascarabotti@gmail.com>
2022-10-13 11:50:20 -0300

    Agregue comentario que explican lo que hice

--- index.js
+++ index.js
@@ -4,24 +4,33 @@ const botones = document.querySelectorAll(".btn");
 
 let operacionCompleta = false;
 
+//Al total de caractener le resto 1 para obtener el indice del último numero (indice se cuenta desde 0, y los caracteres desde 1)
 const ultimoValor = () => displayOperacion.textContent.substring(displayOperacion.textContent.length-1)
 
 const escribirOperacion = text =>{
-    if(displayOperacion.textContent=='_' && text != '.') displayOperacion.textContent = '';
 
+    //Cuando presiono un boton, se borra "_" 
+    if(displayOperacion.textContent=='_' && text != '.') displayOperacion.textContent = '';
+    
+    //Si ingresa un operador (not a number) sigue el cálculo
     if(operacionCompleta && isNaN(text)){
         displayOperacion.textContent = displayResultado.textContent;
         operacionCompleta = false;
     }
 
+    //Si ingresa un número luego de haber clickeado el "=", termina el cálculo y el display vuelve a 0 (porque al apretar "=" se ejecuta la función escribirResultado() y "operacionCompleta" pasa a ser True)
     if(operacionCompleta && !isNaN(text)){
         displayOperacion.textContent = '';
         displayResultado.textContent = '0';
         operacionCompleta = false;
     }
 
+    //Si pongo dos operadores seguidos, el 1ro es reemplazado por el segundo
     if(isNaN(ultimoValor()) && isNaN(text)){
-       displayOperacion.textContent = displayOperacion.textContent.substring(0,displayOperacion.textContent.length-1)
+       displayOperacion.textContent = displayOperacion.textContent.substring(0,displayOperacion.textContent.length-1);
+       displayOperacion.textContent +=text;
+
+    //Limito el largo de caracteres a 25
     }else if(displayOperacion.textContent.length<25){
         displayOperacion.textContent += text;
     }
@@ -36,6 +45,7 @@ const borrarTodo = () =>{
     displayOperacion.textContent = '_';
     displayResultado.textContent = '0';
 }
+
 botones.forEach (boton => {
     boton.addEventListener ('click', e => {
         if(e.target.textContent !== ''){
@@ -49,6 +59,7 @@ botones.forEach (boton => {
                 case ',': 
                     escribirOperacion('.');
                     break;
+            
                 default: 
                     escribirOperacion(e.target.textContent);
                     break;
